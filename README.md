1abe4ff5492c2f00863ab9a3e4e7897fd1c5a5d6
phamconganh <conganhphamnd@gmail.com>
2018-10-09 17:26:05 +0700

    update server

--- README.md
+++ README.md
@@ -25,3 +25,6 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Nodejs connect Mysql
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'YourRootPassword';
--- server/db/scriptDB.sql
+++ server/db/scriptDB.sql
@@ -5,75 +5,80 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema learnEnglishOnline
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema learnEnglishOnline
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `learnEnglishOnline` DEFAULT CHARACTER SET utf8 ;
+USE `learnEnglishOnline` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`user`
+-- Table `learnEnglishOnline`.`level`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `lession` LONGTEXT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`level`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`level` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `learnEnglishOnline`.`level` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
-  `description` TINYTEXT NULL,
+  `description` TINYTEXT NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`lession`
+-- Table `learnEnglishOnline`.`lession`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`lession` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `learnEnglishOnline`.`lession` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   `image_link` TINYTEXT NOT NULL,
-  `level_id` INT NOT NULL,
+  `level_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_lession_level_idx` (`level_id` ASC) VISIBLE,
   CONSTRAINT `fk_lession_level`
     FOREIGN KEY (`level_id`)
-    REFERENCES `mydb`.`level` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `learnEnglishOnline`.`level` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`exercise`
+-- Table `learnEnglishOnline`.`exercise`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`exercise` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `learnEnglishOnline`.`exercise` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `question` MEDIUMTEXT NOT NULL,
-  `type` INT NOT NULL,
+  `type` INT(11) NOT NULL,
   `right_answer` MEDIUMTEXT NOT NULL,
-  `answer` LONGTEXT NULL,
-  `lession_id` INT NOT NULL,
+  `answer` LONGTEXT NULL DEFAULT NULL,
+  `lession_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_exercise_lession1_idx` (`lession_id` ASC) VISIBLE,
   CONSTRAINT `fk_exercise_lession1`
     FOREIGN KEY (`lession_id`)
-    REFERENCES `mydb`.`lession` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `learnEnglishOnline`.`lession` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `learnEnglishOnline`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `learnEnglishOnline`.`user` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `lession` LONGTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `email`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
