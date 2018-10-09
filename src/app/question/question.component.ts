6db43fffbd9ffcf83ce372132980dd913b20c9e6
Hung The Pham <hungpt58.uet@gmail.com>
2018-10-09 20:43:20 +0700

    update

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -9,10 +9,18 @@ import {Question} from '../model/question';
 export class QuestionComponent implements OnInit {
   hearts: Array<string>;
   question: Question = new Question();
+  selectedImage: string;
+  selectedIndex: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.selectedIndex = -1;
     this.hearts = ['black', 'red', 'red'];
+    this.selectedImage = '5px 10px 50px #a9a555';
   }
 
+  onSelectAnswer(index: number): void {
+    this.selectedIndex = index;
+  }
 }
