5865ff4968272a1a833686cda774d523922ae6a3
phamconganh <conganhphamnd@gmail.com>
2018-10-14 18:26:18 +0700

    update login register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LessonListComponent } from './lesson-list/lesson-list.component';
 import { ImageQuestionComponent } from './image-question/image-question.component';
 import {FormsModule} from '@angular/forms';
 import { AccountComponent } from './account/account.component';
+import { EditAccountComponent } from './edit-account/edit-account.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { AccountComponent } from './account/account.component';
     QuestionComponent,
     LessonListComponent,
     ImageQuestionComponent,
-    AccountComponent
+    AccountComponent,
+    EditAccountComponent
   ],
   imports: [
     BrowserModule,
